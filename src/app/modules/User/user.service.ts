f37fc76409b7b76f74027d21818a3fc0fcaf29c0
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-29 19:46:19 +0600

    Implementation Of Password Hashing

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,13 +1,17 @@
 import { PrismaClient, UserRole } from "@prisma/client";
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 const createAdmin = async (data: any) => {
-    console.log(data);
+    
+    const hashedPassword: string = await bcrypt.hash(data.password, 12);
+
+    // console.log(hashedPassword);
 
     const userData = {
         email: data.admin.email,
-        password: data.password,
+        password: hashedPassword,
         role: UserRole.ADMIN,
     };
 
