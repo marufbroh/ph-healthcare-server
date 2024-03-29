70cc5f5210ad22178e4f12069c1af0590c401b78
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-29 19:38:37 +0600

    User & Admin Creation: Part 2

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,7 +1,32 @@
-const createAdmin = async () => {
-    return {
-        message: "Admin Created"
-    }
+import { PrismaClient, UserRole } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const createAdmin = async (data: any) => {
+    console.log(data);
+
+    const userData = {
+        email: data.admin.email,
+        password: data.password,
+        role: UserRole.ADMIN,
+    };
+
+
+    const result = await prisma.$transaction(async (transactionClient) => {
+        await transactionClient.user.create({
+            data: userData
+        });
+
+        const createdAdminData = await transactionClient.admin.create({
+            data: data.admin
+        });
+
+        return createdAdminData
+    });
+
+
+
+    return result;
 };
 
 export const userService = {
