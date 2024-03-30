c1bc5e239511608ce297a8e7b7f9fd809851be26
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-30 19:49:03 +0600

    Optimization Of Search Code: Part 2

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -4,23 +4,17 @@ const prisma = new PrismaClient();
 
 const getAllFromDB = async (params: any) => {
     const andConditions: Prisma.AdminWhereInput[] = [];
+    const adminSearchableFields = ['name', 'email'];
 
     if (params.searchTerm) {
         andConditions.push({
-            OR: [
-                {
-                    name: {
-                        contains: params.searchTerm,
-                        mode: 'insensitive'
-                    }
-                },
-                {
-                    email: {
-                        contains: params.searchTerm,
-                        mode: 'insensitive'
-                    }
+            OR: adminSearchableFields.map((field) => ({
+                [field]: {
+                    contains: params.searchTerm,
+                    mode: 'insensitive'
                 }
-            ]
+
+            }))
         })
     };
 
