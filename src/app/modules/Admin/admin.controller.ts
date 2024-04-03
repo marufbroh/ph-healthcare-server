24a69017df0f306a86656c196f02d9e6f32b32a2
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-04-03 12:38:45 +0600

    Reduce Try-Catch Redundancy

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,112 +1,81 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { AdminService } from "./admin.service";
 import pick from "../../../shared/pick";
 import { adminFilterableFields } from "./admin.constant";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import catchAsync from "../../../shared/catchAsync";
 
 
-const getAllFromDB = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const filters = pick(req.query, adminFilterableFields);
-        const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+const getAllFromDB = catchAsync(async (req, res) => {
+    const filters = pick(req.query, adminFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
 
+    const result = await AdminService.getAllFromDB(filters, options);
 
-        const result = await AdminService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Admin retrieve successfully",
+        meta: result.meta,
+        data: result.data
+    });
 
-        // res.status(200).json({
-        //     success: true,
-        //     message: "Admin retrieve successfully",
-        //     meta: result.meta,
-        //     data: result.data
-        // })
+});
 
-        sendResponse(res, {
-            statusCode: httpStatus.OK,
-            success: true,
-            message: "Admin retrieve successfully",
-            meta: result.meta,
-            data: result.data
-        });
+const getByIdFromDB = catchAsync(async (req, res) => {
+    const id = req.params.id;
+    const result = await AdminService.getByIdFromDB(id)
 
-    } catch (error) {
-        next(error)
-    }
-};
-
-const getByIdFromDB = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const id = req.params.id;
-        const result = await AdminService.getByIdFromDB(id)
-
-        sendResponse(res, {
-            statusCode: httpStatus.OK,
-            success: true,
-            message: "Admin retrieve successfully by Id",
-            data: result
-        });
-
-    } catch (error) {
-        next(error)
-    }
-};
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Admin retrieve successfully by Id",
+        data: result
+    });
+});
 
 
 
-const updateIntoDB = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const id = req.params.id;
+const updateIntoDB = catchAsync(async (req, res) => {
+    const id = req.params.id;
 
-        const result = await AdminService.updateIntoDB(id, req.body)
+    const result = await AdminService.updateIntoDB(id, req.body)
 
-        sendResponse(res, {
-            statusCode: httpStatus.OK,
-            success: true,
-            message: "Admin data updated",
-            data: result
-        });
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Admin data updated",
+        data: result
+    });
+});
 
-    } catch (error) {
-        next(error)
-    }
-};
+const deleteFromDB = catchAsync(async (req, res) => {
+    const id = req.params.id;
 
-const deleteFromDB = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const id = req.params.id;
+    const result = await AdminService.deleteFromDB(id)
 
-        const result = await AdminService.deleteFromDB(id)
-
-        sendResponse(res, {
-            statusCode: httpStatus.OK,
-            success: true,
-            message: "Admin data deleted",
-            data: result
-        });
-
-    } catch (error) {
-        next(error)
-    }
-};
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Admin data deleted",
+        data: result
+    });
+});
 
 
-const softDeleteFromDB = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const id = req.params.id;
+const softDeleteFromDB = catchAsync(async (req, res) => {
+    const id = req.params.id;
 
-        const result = await AdminService.softDeleteFromDB(id)
+    const result = await AdminService.softDeleteFromDB(id)
 
-        sendResponse(res, {
-            statusCode: httpStatus.OK,
-            success: true,
-            message: "Admin data soft deleted",
-            data: result
-        });
-
-    } catch (error) {
-        next(error)
-    }
-};
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Admin data soft deleted",
+        data: result
+    });
+});
 
 
 export const AdminController = {
