3c7eb025f3e09f09d63a45bed199a3fa8143a6e2
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-04-04 16:13:34 +0600

    Developing Reusable Middleware

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,32 +1,14 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AdminController } from './admin.controller';
-import { AnyZodObject, ZodObject, z } from 'zod';
+import { adminValidationSchemas } from './admin.validations';
 
 const router = express.Router();
 
-const updateAdmin = z.object({
-    body: z.object({
-        name: z.string().optional(),
-        contactNumber: z.string().optional()
-    })
-})
-
-const validateRequest = (schema: AnyZodObject) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            await schema.parseAsync({
-                body: req.body
-            })
-            return next();
-        } catch (error) {
-            next(error)
-        }
-    }
-}
 
 router.get("/", AdminController.getAllFromDB)
 router.get("/:id", AdminController.getByIdFromDB)
-router.patch("/:id", validateRequest(updateAdmin), AdminController.updateIntoDB)
+router.patch("/:id", validateRequest(adminValidationSchemas.updateAdmin), AdminController.updateIntoDB)
 router.delete("/:id", AdminController.deleteFromDB)
 router.delete("/soft/:id", AdminController.softDeleteFromDB)
 
