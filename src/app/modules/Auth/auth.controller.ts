17ae29bfe225bf0540ced8dae5595a9105ab01ca
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-04-05 11:27:06 +0600

    Generating Access Tokens For User Login

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -3,8 +3,8 @@ import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import { authServices } from "./auth.service";
 
-const loginUser = catchAsync(async (req, res, next) => {
-    const result = authServices.loginUser(req.body);
+const loginUser = catchAsync(async (req, res) => {
+    const result = await authServices.loginUser(req.body);
 
     sendResponse(res, {
         statusCode: httpStatus.OK,
--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,8 +1,33 @@
+import prisma from "../../../shared/prisma";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 const loginUser = async (payload: {
     email: string,
     password: string
 }) => {
-    console.log("USer logged in...", payload);
+    const userData = await prisma.user.findUniqueOrThrow({
+        where: {
+            email: payload.email
+        }
+    });
+
+    const isCorrectPassword: Boolean = await bcrypt.compare(payload.password, userData.password);
+
+    const accessToken = jwt.sign(
+        {
+            email: userData.email,
+            role: userData.role
+        },
+        "abcdefg",
+        {
+            algorithm: 'HS256',
+            expiresIn: '15m'
+        });
+
+    console.log(accessToken);
+
+    return userData;
 };
 
 export const authServices = {
