a24695d098aef2df46b933e610747d00833a105d
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-04-06 17:51:25 +0600

    Reusable Authentication Middleware And Handling API Errors

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,35 +1,9 @@
-import express, { NextFunction, Request, Response } from 'express';
-import { userController } from './user.controller';
 import { UserRole } from '@prisma/client';
-import { jwtHelpers } from '../../../helpers/jwtHelpers';
-import config from '../../../config';
-import { Secret } from 'jsonwebtoken';
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { userController } from './user.controller';
 
 const router = express.Router();
-
-const auth = (...roles: string[]) => {
-
-    return async (req: Request, res: Response, next: NextFunction) => {
-
-        try {
-            const token = req.headers.authorization;
-
-            if (!token) {
-                throw new Error("You're not authorized!")
-            }
-            const verifiedUser = jwtHelpers.verifyToken(token, config.jwt.access_token_secret as Secret);
-
-            if(roles.length && !roles.includes(verifiedUser.role)){
-                throw new Error("You're not authorized!")
-            }
-            
-            next();
-        } catch (error) {
-            next(error);
-        }
-    }
-
-}
 
 router.post("/", auth(UserRole.SUPER_ADMIN, UserRole.ADMIN), userController.createAdmin)
 
