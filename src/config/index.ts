3b1fc5df61ea31b03fec22805191bc117b0712b3
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-04-08 19:51:43 +0600

    Defining Types For File And Cloudinary Response

--- src/config/index.ts
+++ src/config/index.ts
@@ -19,5 +19,10 @@ export default {
     emailSender: {
         email: process.env.EMAIL,
         app_pass: process.env.APP_PASS
+    },
+    cloudinary: {
+        cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+        cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
     }
 }
--- src/helpers/fileUploader.ts
+++ src/helpers/fileUploader.ts
@@ -2,8 +2,14 @@ import multer from "multer"
 import path from "path"
 import fs from 'fs';
 import { v2 as cloudinary } from 'cloudinary';
+import config from "../config";
+import { ICloudinaryResponse, IFile } from "../app/interfaces/file";
 
-
+cloudinary.config({
+    cloud_name: config.cloudinary.cloudinary_cloud_name,
+    api_key: config.cloudinary.cloudinary_api_key,
+    api_secret: config.cloudinary.cloudinary_api_secret
+});
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -17,12 +23,11 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 
-const uploadToCloudinary = async (file: any) => {
+const uploadToCloudinary = async (file: IFile): Promise<ICloudinaryResponse | undefined> => {
 
     return new Promise((resolve, reject) => {
         cloudinary.uploader.upload(file.path,
-            { public_id: file.originalname },
-            (error, result) => {
+            (error: Error, result: ICloudinaryResponse) => {
                 fs.unlinkSync(file.path)
                 if (error) {
                     reject(error)
