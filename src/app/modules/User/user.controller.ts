e0ecbc824ed6a781b1b8ebe9fcbaf9f06e6b49eb
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-04-13 22:08:59 +0600

    removed  get me controller

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
-import { userService } from "./user.service";
-import catchAsync from "../../../shared/catchAsync";
-import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import catchAsync from "../../../shared/catchAsync";
 import pick from "../../../shared/pick";
+import sendResponse from "../../../shared/sendResponse";
 import { userFilterableFields } from "./user.constant";
-import { IAuthUser } from "../../interfaces/common";
+import { userService } from "./user.service";
 
 const createAdmin = catchAsync(async (req: Request, res: Response) => {
 
@@ -69,26 +68,10 @@ const {id} = req.params;
 });
 
 
-const getMyProfile = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
-
-    const user = req.user;
-
-    const result = await userService.getMyProfile(user as IAuthUser);
-
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "My profile data fetched!",
-        data: result
-    })
-});
-
-
 export const userController = {
     createAdmin,
     createDoctor,
     createPatient,
     getAllFromDB,
     changeProfileStatus,
-    getMyProfile,
 }
