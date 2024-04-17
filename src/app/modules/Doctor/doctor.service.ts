9174bcc317f7f7f6909f6fa42c6dc5272363812f
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-04-17 19:36:24 +0600

    Updating Doctor Data

--- src/app/modules/Doctor/doctor.service.ts
+++ src/app/modules/Doctor/doctor.service.ts
@@ -1,5 +1,20 @@
-const updateIntoDB = async (id: string, payload: any) => {
+import prisma from "../../../shared/prisma"
 
+const updateIntoDB = async (id: string, payload: any) => {
+    const doctorData = await prisma.doctor.findUniqueOrThrow({
+        where: {
+            id: id
+        }
+    });
+
+    const updatedDoctorData = prisma.doctor.update({
+        where: {
+            id
+        },
+        data: payload
+    });
+
+    return updatedDoctorData;
 }
 
 export const DoctorService = {
