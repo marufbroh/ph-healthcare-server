0b4cc2f80b75dd36124640e31fb1a8ab8f8107f7
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-04-21 21:35:14 +0600

    get and single controller added

---
+++ src/app/modules/Patient/patient.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import pick from "../../../shared/pick";
+import { patientFilterableFields } from "./patient.constants";
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
+import { PatientService } from "./patient.services";
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, patientFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  
+    const result = await PatientService.getAllFromDB(filters, options);
+  
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Patient retrieval successfully',
+      meta: result.meta,
+      data: result.data,
+    });
+  });
+  
+  const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  
+    const { id } = req.params;
+    const result = await PatientService.getByIdFromDB(id);
+  
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Patient retrieval successfully',
+      data: result,
+    });
+  });
