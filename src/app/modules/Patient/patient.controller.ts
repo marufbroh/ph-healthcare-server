56c2960cf0fe77b0c32e2ec08f1e1b8c25aa940a
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-04-23 18:26:01 +0600

    update controller added

--- src/app/modules/Patient/patient.controller.ts
+++ src/app/modules/Patient/patient.controller.ts
@@ -1,66 +1,78 @@
-import { Request, Response } from "express";
-import catchAsync from "../../../shared/catchAsync";
-import pick from "../../../shared/pick";
-import { patientFilterableFields } from "./patient.constants";
-import sendResponse from "../../../shared/sendResponse";
-import httpStatus from "http-status";
-import { PatientService } from "./patient.services";
+import { Request, Response } from 'express';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
+import pick from '../../../shared/pick';
+import { patientFilterableFields } from './patient.constants';
+import { PatientService } from './patient.services';
 
 const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
-    const filters = pick(req.query, patientFilterableFields);
-    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
-  
-    const result = await PatientService.getAllFromDB(filters, options);
-  
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Patient retrieval successfully',
-      meta: result.meta,
-      data: result.data,
-    });
+  const filters = pick(req.query, patientFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+  const result = await PatientService.getAllFromDB(filters, options);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Patient retrieval successfully',
+    meta: result.meta,
+    data: result.data,
   });
-  
-  const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
-  
-    const { id } = req.params;
-    const result = await PatientService.getByIdFromDB(id);
-  
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Patient retrieval successfully',
-      data: result,
-    });
+});
+
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+
+  const { id } = req.params;
+  const result = await PatientService.getByIdFromDB(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Patient retrieval successfully',
+    data: result,
   });
+});
 
-  const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const result = await PatientService.deleteFromDB(id);
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Patient deleted successfully',
-      data: result,
-    });
+const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await PatientService.updateIntoDB(id, req.body);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Patient updated successfully',
+    data: result,
   });
+});
 
-
-  const softDelete = catchAsync(async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const result = await PatientService.softDelete(id);
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Patient soft deleted successfully',
-      data: result,
-    });
+const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await PatientService.deleteFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Patient deleted successfully',
+    data: result,
   });
+});
 
 
-  export const PatientController = {
-    getAllFromDB,
-    getByIdFromDB,
-    deleteFromDB,
-    softDelete
-  };
+const softDelete = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await PatientService.softDelete(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Patient soft deleted successfully',
+    data: result,
+  });
+});
+
+export const PatientController = {
+  getAllFromDB,
+  getByIdFromDB,
+  updateIntoDB,
+  deleteFromDB,
+  softDelete,
+};
